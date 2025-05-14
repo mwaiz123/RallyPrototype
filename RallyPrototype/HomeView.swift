244deab7b72efd6044f9826dead35589f917510b
Mansib <mansibwaiz123@gmail.com>
2025-05-14 02:14:41 -0400

    Designed the basic functionality of the sheet which will hold the creation button as well as a list of possible events

--- RallyPrototype/HomeView.swift
+++ RallyPrototype/HomeView.swift
@@ -10,30 +10,50 @@ import MapKit
 
 struct HomeView: View {
     @State private var showSheet: Bool = false
-    let locationManager = CLLocationManager()
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
     
     var body: some View {
         NavigationStack {
-            MapView()
-                .task {
-                    showSheet = true
-                }
-                .sheet(isPresented: $showSheet, content: {
-                    VStack {
-                        Text("Placeholder")
+            Map(position: $position) {
+                
+            }
+            .mapControls {
+                MapUserLocationButton()
+                MapPitchToggle()
+            }
+            .onAppear {
+                CLLocationManager().requestWhenInUseAuthorization()
+            }
+            .task {
+                showSheet = true
+            }
+            .sheet(isPresented: $showSheet, content: {
+                VStack {
+                    Button(action: {}) {
+                        Text("Create Event")
+                            .font(.title)
+                            .padding(.vertical, 20)
+                            .frame(maxWidth: .infinity) // Full-width button
+                            .background(RoundedRectangle(cornerRadius: 20).fill(Color.purple))
+                            .foregroundColor(.white)
                     }
-                    .padding()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                    .presentationDetents([.height(280), .medium, .large])
-                    .presentationCornerRadius(28)
-                    .presentationBackground(.white)
-                    .presentationBackgroundInteraction(.enabled(upThrough: .large))
-                    .interactiveDismissDisabled(true)
-                })
+                    .padding(.horizontal, 0)
+                    .padding(.vertical, 5)
+                    
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .presentationDetents([.height(280), .medium, .large])
+                .presentationCornerRadius(28)
+                .presentationBackground(.white)
+                .presentationBackgroundInteraction(.enabled(upThrough: .large))
+                .interactiveDismissDisabled(true)
+            })
         }
+        .navigationBarHidden(true)
     }
 }
-
 
 #Preview {
     HomeView()
