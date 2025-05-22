78048d2f0349c2f06df98ba79aa3c8bac1535e58
Mansib <mansibwaiz123@gmail.com>
2025-05-22 11:44:54 -0400

    Removal of Navigation Button

--- RallyPrototype/MapView.swift
+++ RallyPrototype/MapView.swift
@@ -14,33 +14,12 @@ struct MapView: View {
     @State private var position: MapCameraPosition = .automatic
     @State private var userCoordinate: CLLocationCoordinate2D?
     @State private var searchText = ""
-    @State private var isNavigating = false
 
     var body: some View {
-        ZStack(alignment: .topLeading) {
             Map(position: $position) {}
             .ignoresSafeArea()
 
-            HStack {
-                Button {
-                    isSheetPresented = false
-                    isNavigating = true
-                } label: {
-                    Image(systemName: "magnifyingglass")
-                        .font(.title3)
-                        .foregroundColor(.primary)
-                        .padding()
-                        .background(Color.white)
-                        .clipShape(Circle())
-                        .shadow(radius: 2)
-                }
-                .padding(.top, 30)
-                .padding(.leading, 20)
-            }
-        }
-        .navigationDestination(isPresented: $isNavigating) {
-            SearchView()
-        }
+           
         .onAppear {
             let manager = CLLocationManager()
             manager.requestWhenInUseAuthorization()
