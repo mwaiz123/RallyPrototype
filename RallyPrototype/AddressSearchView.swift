2d48be1078488e08166f79f875ffa3b695e44aec
Mansib <mansibwaiz123@gmail.com>
2025-05-29 16:52:13 -0400

    Designed Event Creation View

---
+++ RallyPrototype/AddressSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressSearchView.swift
+//  RallyPrototype
+//
+//  Created by Mansib Waiz on 2025-05-28.
+//
+
+import SwiftUI
+
+struct AddressSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressSearchView()
+}
---
+++ RallyPrototype/EventCreationView.swift
@@ -0,0 +1,18 @@
+//
+//  EventCreationView.swift
+//  RallyPrototype
+//
+//  Created by Mansib Waiz on 2025-05-22.
+//
+
+import SwiftUI
+
+struct EventCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventCreationView()
+}
