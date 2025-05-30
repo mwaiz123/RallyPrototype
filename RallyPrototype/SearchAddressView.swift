36c74e574327eafc4c6745f02ff29c27047828f9
Mansib <mansibwaiz123@gmail.com>
2025-05-30 12:22:50 -0400

    Created

---
+++ RallyPrototype/SearchAddressView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAddressView.swift
+//  RallyPrototype
+//
+//  Created by Mansib Waiz on 2025-05-30.
+//
+
+import SwiftUI
+
+struct SearchAddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAddressView()
+}
